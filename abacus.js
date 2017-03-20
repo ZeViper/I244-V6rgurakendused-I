7517a1777d08d7fcb8ccbb4b6f63a9c75cabd610
Egert Koll <Deagle95@hotmail.com>
2017-03-20 02:58:10 +0200

    edited abacus.js

---
+++ abacus.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  
+  var beads = document.querySelectorAll('.bead');
+  
+  for( i = 0; i < beads.length; i++ ) {
+	
+	beads[ i ].onclick = function() {
+	  
+	  var beadFloat = window.getComputedStyle( this, null ).cssFloat;
+	  
+	  if ( beadFloat == "right" ) {
+		  
+		  this.style.cssFloat = "left";
+		  
+	  } else {
+		  
+		  this.style.cssFloat = "right";
+		  
+	  }
+	}
+  }
+}
