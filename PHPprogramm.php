e06837a6a0affe98339f5e88fdef08cbad7da620
Egert Koll <Deagle95@hotmail.com>
2017-04-09 22:38:48 +0300

    added PHPprogramm.php

---
+++ PHPprogramm.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+		<meta charset="utf-8" />
+		<title>Ülesanne 8</title>
+		<style type="text/css">
+			
+		</style>
+  </head>
+  <body>
+		<div>
+			
+			<p></p>
+			
+		</div>
+		<div>
+			
+			
+			
+		</div>
+		<?php
+	
+	  
+	
+		?>
+  </body>
+</html>
